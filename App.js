62861afec5f51d2c2ac5c7931c802edae5e050cc
Dennys Lopez <dlop@hcastechnology.com>
2019-01-24 12:33:32 -0500

    Props   - Implemented a Presentational Component to present info and declare user interation   - Passed secondary state as props and updated it through PresentationalComponent

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import PresentationalComponent from './PresentationalComponent';
+
 type Props = {};
 export default class App extends Component<Props> {
   state = {
@@ -33,12 +35,11 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native Tutorial</Text>
-        <Text onPress={this.updateState}>
-          {this.state.myState}
-        </Text>
+        <PresentationalComponent myState={this.state.myState} updateState={this.updateState} />
         <Text onPress={this.updateAnotherState}>
           {this.state.anotherState}
         </Text>
+
       </View>
     );
   }
